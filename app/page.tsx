e725d389ddacbb0d93fc686d8ea9e8434142bccd
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2024-12-07 09:03:59 +0530

    what is it

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import background from "@/photos/background.jpeg";
 //import { CountdownTimer } from "@/components/countdown-timer";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-centry justify-center relative overflow-hidden">
-      <Image src="" alt="" fill className="object-cover" priority />
+      <Image src={background} alt="" fill className="object-cover" priority />
       <div className="absolute inset-0 bg-black bg-opactity-50" />
       <div className="z-10 text-center px-4">
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-white mb-4">

284b6facbc5b3ee9226a0de94ccb35c6a8b0bf4c
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2024-12-15 12:14:50 +0530

    adding new image

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,21 @@ import background from "@/photos/background.jpg";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-centry justify-center relative overflow-hidden">
-      <div className="absolute inset-0 bg-white bg-opactity-50" />
+      <div className="absolute inset-0 bg-white opacity-10 " />
       <Image
         src={background}
         alt="Travel Spots"
         fill
-        className="object-cover"
+        className="object-cover opacity-30 "
         priority
       />
       <div className="z-40 text-center px-9 py-10">
-        <h1 className="text-4xl md:text-6xl lg:text-7xl font-extrabold text-amber-300 antialiased mb-4">
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-extrabold text-red-500 text-center antialiased mb-4">
           Destination Sikkim
         </h1>
-        <h2 className="text-xl md:text-2xl lg:text-3xl text--500 text-blue-300 antialiased placeholder-slate-900 text-center   font-extrabold mb-8">
-          A travel agency coming for all your travel desires across Sikkim
+        <h2 className="text-xl md:text-2xl lg:text-3xl text--500 text-blue-400 antialiased placeholder-slate-900 text-center   font-extrabold mb-8">
+          <strong> A travel agency</strong> <br></br>
+          for all travel desires across sikkim
         </h2>
       </div>
     </main>
